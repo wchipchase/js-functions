c9aeebfd8661f65b695d9c65b624c28adb149784
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-03-02 11:07:27 -0600

    nameprinter function works

--- main.js
+++ main.js
@@ -2,4 +2,17 @@
 
 const firstName = 'Wayne';
 const lastName = 'Chipchase';
-console.log(firstName + ' ' + lastName);
+// console.log(firstName + ' ' + lastName);
+console.log(`${firstName} ${lastName}`);
+
+const firstName1 = 'Britney';
+const lastName1 = 'Spears';
+
+
+const namePrinter = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`);
+
+};
+
+namePrinter('Zoe', 'Ames');
+namePrinter('John', 'Doe');
